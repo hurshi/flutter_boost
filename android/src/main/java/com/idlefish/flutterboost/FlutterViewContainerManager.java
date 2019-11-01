2d0d5bce85894befafaaa0b9e3494224aaebe284
Hurshi <ihsruh@gmail.com>
2019-11-01 14:39:39 +0800

    fix androidx and result

--- android/src/main/java/com/idlefish/flutterboost/FlutterViewContainerManager.java
+++ android/src/main/java/com/idlefish/flutterboost/FlutterViewContainerManager.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2019 Alibaba Group
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,6 +27,7 @@ import android.content.Context;
 import android.text.TextUtils;
 import android.util.SparseArray;
 
+import com.idlefish.flutterboost.containers.NewBoostFlutterActivity;
 import com.idlefish.flutterboost.interfaces.IContainerManager;
 import com.idlefish.flutterboost.interfaces.IContainerRecord;
 import com.idlefish.flutterboost.interfaces.IFlutterViewContainer;
@@ -135,8 +136,20 @@ public class FlutterViewContainerManager implements IContainerManager {
 
         final String uniqueId = ContainerRecord.genUniqueId(url);
         urlParams.put(IContainerRecord.UNIQ_KEY,uniqueId);
+//        if(onResult != null) {
+//            mOnResults.put(uniqueId,onResult);
+//        }
         if(onResult != null) {
-            mOnResults.put(uniqueId,onResult);
+            String uId = "";
+            if (NewFlutterBoost.instance().currentActivity() instanceof NewBoostFlutterActivity) {
+                Map params = ((NewBoostFlutterActivity) NewFlutterBoost.instance().currentActivity()).getContainerUrlParams();
+                Object uIdOjb = params.get(IContainerRecord.UNIQ_KEY);
+                if(null!=uIdOjb) uId = uIdOjb.toString();
+            }
+            if( null == uId || uId.length() <= 0 ) {
+                uId = NewFlutterBoost.instance().containerManager().getLastGenerateRecord().uniqueId();
+            }
+            mOnResults.put(uId, onResult);
         }
 
         NewFlutterBoost.instance().platform().openContainer(context,url,urlParams,requestCode,exts);
